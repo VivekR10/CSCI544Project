990036a09eecee7f5a911203ba688c93372a4683
PrasanthKongara <54452962+PrasanthKongara@users.noreply.github.com>
2019-11-25 18:58:15 -0800

    updated params in mymodel

--- DIIN/DIIN_PyTorch/pytorch/train_snli_pytorch.py
+++ DIIN/DIIN_PyTorch/pytorch/train_snli_pytorch.py
@@ -452,7 +452,7 @@ completed = False
 
 
 
-model = MyModel(logger,config, FIXED_PARAMETERS["seq_length"], emb_dim=FIXED_PARAMETERS["word_embedding_dim"],  hidden_dim=FIXED_PARAMETERS["hidden_embedding_dim"], embeddings=loaded_embeddings, emb_train=FIXED_PARAMETERS["emb_train"],indices_to_words)
+model = MyModel(logger,indices_to_words,config, FIXED_PARAMETERS["seq_length"], emb_dim=FIXED_PARAMETERS["word_embedding_dim"],  hidden_dim=FIXED_PARAMETERS["hidden_embedding_dim"], embeddings=loaded_embeddings, emb_train=FIXED_PARAMETERS["emb_train"])
 
 if config.cuda:
     model.cuda()
